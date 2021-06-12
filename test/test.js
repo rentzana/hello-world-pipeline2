eabb0cf357ca56ddb15a9254ce4e3288c32efa09
Ramon Entzana Muciño <rentzana@gmail.com>
2021-06-11 23:40:05 -0500

    five content change

--- test/test.js
+++ test/test.js
@@ -1,8 +1,9 @@
 var request = require('supertest');
 var app = require('../index.js')
 describe('GET /', function () {
-    it('displays "Hello World 2!"', function (done) {
+    it('displays "Title Hello World 2!"', function (done) {
         // The line below is the core test of our app.
-        request(app).get('/').expect('Hello World 1!', done);
+        // Change this content for proofs
+        request(app).get('/').expect('Hello World 2!', done);
     });
 });
